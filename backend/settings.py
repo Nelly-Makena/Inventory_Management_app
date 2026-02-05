0c9c72375be6889c07bd9b521d46a73b15ee70b8
NellyMakena <nellysolomonmakena3@gmail.com>
2026-02-05 15:03:04 +0300

    fix

--- backend/settings.py
+++ backend/settings.py
@@ -55,9 +55,25 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 ROOT_URLCONF = 'backend.urls'
 AUTH_USER_MODEL = 'accounts.User'
 REST_FRAMEWORK = {
